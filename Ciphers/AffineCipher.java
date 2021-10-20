89ff3127da208e8d87d16a090d156496a5e74a9c
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-10-20 17:31:38 +0530

    Update AffineCipher.java

--- Ciphers/AffineCipher.java
+++ Ciphers/AffineCipher.java
@@ -1,24 +1,14 @@
-//The ‘key’ for the Affine cipher consists of 2 numbers, we’ll call them a and b.
-// The following discussion assumes the use of a 26 character alphabet (m = 26).
-// a should be chosen to be relatively prime to m (i.e. a should have no factors in common with m).
-
 package Ciphers;
-
-import java.util.Scanner;
-
-class AffineCipher
+class affineCipher
 {
-    static Scanner in = new Scanner(System.in);
-
+ 
+    // Key values of a and b
+    static int a = 17;
+    static int b = 20;
+ 
     static String encryptMessage(char[] msg)
     {
-        System.out.println("Enter key value a for encryption : ");
-        int a = in.nextInt();
-        
-        System.out.println("Enter key value b for encryption : ");
-        int b = in.nextInt();
-        
-        /// Initially empty cipher String
+        /// Cipher Text initially empty
         String cipher = "";
         for (int i = 0; i < msg.length; i++)
         {
@@ -30,34 +20,28 @@ class AffineCipher
             {
                 cipher = cipher
                         + (char) ((((a * (msg[i] - 'A')) + b) % 26) + 'A');
-            } else // append space character
+            } else // else simply append space character
             {
                 cipher += msg[i];
             }
         }
         return cipher;
     }
-
+ 
     static String decryptCipher(String cipher)
     {
-        System.out.println("Enter key value a for decryption : ");
-        int a = in.nextInt();
-        
-        System.out.println("Enter key value b for decryption : ");
-        int b = in.nextInt();
-        
         String msg = "";
         int a_inv = 0;
         int flag = 0;
-
+ 
         //Find a^-1 (the multiplicative inverse of a
         //in the group of integers modulo m.)
         for (int i = 0; i < 26; i++)
         {
             flag = (a * i) % 26;
-
+ 
             // Check if (a*i)%26 == 1,
-            // if so, then i will be the multiplicative inverse of a
+            // then i will be the multiplicative inverse of a
             if (flag == 1)
             {
                 a_inv = i;
@@ -73,26 +57,27 @@ class AffineCipher
                 msg = msg + (char) (((a_inv *
                         ((cipher.charAt(i) + 'A' - b)) % 26)) + 'A');
             }
-            else // append space character
+            else //else simply append space character
             {
                 msg += cipher.charAt(i);
             }
         }
-
+ 
         return msg;
     }
-
-    // Main method
+ 
+    // Driver code
     public static void main(String[] args)
     {
         String msg = "AFFINE CIPHER";
-
-        // Encrypting message
+ 
+        // Calling encryption function
         String cipherText = encryptMessage(msg.toCharArray());
         System.out.println("Encrypted Message is : " + cipherText);
-
-        // Decrypting message
+ 
+        // Calling Decryption function
         System.out.println("Decrypted Message is: " + decryptCipher(cipherText));
-
+ 
     }
 }
+ 
